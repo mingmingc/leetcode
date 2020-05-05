d8674d4d2dc4e42ab77f1b434228f8308bd43834
mingmingc <caressi.m@gmail.com>
2020-05-04 20:04:19 -0700

    trapping rain water:

---
+++ trappingRainWater.js
@@ -0,0 +1,32 @@
+function trappingRainWater(rocks) {
+  let water = 0;
+  let maxLeft = [rocks[0]];
+  let maxRight = new Array(rocks.length);
+
+  maxRight.fill(0);
+  maxRight[rocks.length - 1] = rocks[rocks.length-1];
+  //loop thru rocks
+  //get max left of each rock
+  for (let i=1; i<rocks.length; i++) {
+    // let max = maxLeft[i-1] > rocks[i] ? maxLeft[i-1] : rocks[i];
+    // maxLeft.push(max);
+    maxLeft[i] = Math.max(maxLeft[i - 1], rocks[i]);
+  }
+  
+  //maxRight
+  for (let j = rocks.length - 2; j >= 0; j--) {
+    // let max = maxLeft[i-1] > rocks[i] ? maxLeft[i-1] : rocks[i];
+    // maxLeft.push(max);
+    maxRight[j] = Math.max(maxRight[j + 1], rocks[j]);
+  }
+  
+  //amt of water: Math.min(MAX LEFT , RIGHT) - col height
+  for (let k=0; k<rocks.length; k++) {
+    water += Math.min(maxLeft[k], maxRight[k]) - rocks[k];
+  }
+
+  return water;
+}
+
+console.log(trappingRainWater([3, 0, 2, 0, 4])); //expect 7
+console.log(trappingRainWater([3, 0, 2, 0, 4, 2, 5])); //9
